5940ddcafc699dffd6ba7a1d2395984d53d7ed42
kombattle <88496886+kombattle@users.noreply.github.com>
2021-10-01 23:12:47 +0700

    done test progeam

--- script.js
+++ script.js
@@ -3,35 +3,41 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-//set 3
+//step up3
 const generateTarget = () => {
-    return Math.floor(Math.random()* 10)
- }
- 
-const compareGuesses = (humanGuess,computerGuess,targetGuess) => {
-    const humanDif = Math.abs(targetGuess - humanGuess);
-    const comDif = Math.abs(targetGuess - computerGuess);
-    if (humanDif < comDif) {
-        return "Human Win"
-    }else if (humanDif === comDif) {
-        return "Human Win"
-    }else {
-        return "Computer Win"
-    }
+   return Math.floor(Math.random()* 10)
+}
+
+//step 4
+let compareGuesses = (humanGuess,computerGuess,targetGuess) => {
+  const humanDif = Math.abs(targetGuess - humanGuess
+  );
+  const computerDif = Math.abs(targetGuess - computerGuess);
+  if (humanDif < computerDif || humanDif === computerDif) {
+    return "Human Win"
+  }
+  else if (humanGuess > 9 || humanGuess < 0) {
+    return alert("you can put number only 0 to 9")
+  }
+  else {
+    return "Computer Win You Noob Shit"
+  }
+
+}
+// step 5
+
+const updateScore = (Winner) => {
+  if (Winner === 'human'){
+    humanScore++
+  }else if (Winner === 'computer'){
+    computerScore++
+  }
+}
+
+//step 6
+
+const advanceRound = () => {
+  currentRoundNumber++
 }
 
 
-let updateScore = (Winner) => {
-    if (Winner === 'human') {
-        humanScore++
-    }else if (Winner === 'computer') {
-        computerScore++
-    }
-}
-updateScore('computer');
-updateScore('computer');
-console.log(computerScore); 
-
-let advanceRound = () => {
-
-}
