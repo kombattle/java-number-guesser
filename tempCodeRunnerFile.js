55007a5b21884c33ccb13fafe162b1bc83a84de9
kombattle <88496886+kombattle@users.noreply.github.com>
2021-10-01 23:12:58 +0700

    .

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//set 3
+const generateTarget = () => {
+    return Math.floor(Math.random()* 10)
+ }
+ 
+const compareGuesses = (humanGuess,computerGuess,targetGuess) => {
+    const humanDif = Math.abs(targetGuess - humanGuess);
+    const comDif = Math.abs(targetGuess - computerGuess);
+    if (humanDif < comDif) {
+        return "Human Win"
+    }else if (humanDif === comDif) {
+        return "Human Win"
+    }else {
+        return "Computer Win"
+    }
+}
+
+
+let updateScore = (Winner) => {
+    if (Winner === 'human') {
+        humanScore++
+    }else if (Winner === 'computer') {
+        computerScore++
+    }
+}
+updateScore('computer');
+updateScore('computer');
+console.log(computerScore); 
