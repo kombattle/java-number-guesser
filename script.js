344aabfcc80e95adeba54c026cfcacd8a950553a
kombattle <88496886+kombattle@users.noreply.github.com>
2021-10-01 20:20:31 +0700

    create project and connect git

---
+++ script.js
@@ -0,0 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+//set 3
+const generateTarget = () => {
+    return Math.floor(Math.random()* 10)
+ }
+ 
